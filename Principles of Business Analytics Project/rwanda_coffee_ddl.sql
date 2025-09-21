03abe314b25579369ecd76b319a6981c83cf6182
rgr-lena1 <mhoranihirwe@gmail.com>
2025-09-21 11:03:48 +0200

    Update rwanda_coffee_ddl.sql

--- Principles of Business Analytics Project/rwanda_coffee_ddl.sql
+++ Principles of Business Analytics Project/rwanda_coffee_ddl.sql
@@ -1,49 +1,46 @@
+-- Create the database schema and tables 
+CREATE SCHEMA rwanda_coffee;
 
--- Farmers table
+-- Farmers table captures information of cooperative members
 CREATE TABLE Farmers (
-    farmer_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    district VARCHAR(50),
+    district VARCHAR(50) NOT NULL,
     cooperative_join_date DATE
+    PRIMARY KEY (farmer_id)
 );
 
--- Buyers table
+-- Buyers table contains information of local and international buyers
 CREATE TABLE Buyers (
-    buyer_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     country VARCHAR(50),
     contract_start_date DATE
+    PRIMARY KEY (buyer_id)
 );
 
--- Harvests table
+-- Harvests table captures each coffee harvest linked to a buyer
 CREATE TABLE Harvests (
-    harvest_id SERIAL PRIMARY KEY,
     farmer_id INT NOT NULL,
     harvest_date DATE NOT NULL,
-    quantity_kg NUMERIC CHECK (quantity_kg > 0),
-    CONSTRAINT fk_farmer
-        FOREIGN KEY (farmer_id)
-        REFERENCES Farmers(farmer_id)
+    quantity_kg NUMERIC (10,2) CHECK (quantity_kg > 0),
+    batch_code VARCHAR(50) UNIQUE NOT NULL,
+    PRIMARY KEY  (harvest_id),
+    FOREIGN KEY (farmer_id) REFERENCES Farmers(farmer_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
--- Sales table
+-- Sales table links harvests with buyers and records transactions
 CREATE TABLE Sales (
-    sale_id SERIAL PRIMARY KEY,
     harvest_id INT NOT NULL,
     buyer_id INT NOT NULL,
     sale_date DATE NOT NULL,
     price_per_kg NUMERIC CHECK (price_per_kg > 0),
     total_amount NUMERIC CHECK (total_amount >= 0),
-    CONSTRAINT fk_harvest
-        FOREIGN KEY (harvest_id)
-        REFERENCES Harvests(harvest_id)
+    PRIMARY KEY (sale_id)
+    FOREIGN KEY (harvest_id) REFERENCES Harvests(harvest_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    CONSTRAINT fk_buyer
-        FOREIGN KEY (buyer_id)
-        REFERENCES Buyers(buyer_id)
+    FOREIGN KEY (buyer_id) REFERENCES Buyers(buyer_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
