69fab5a778689d6769a684d35a8f51bff0ce992a
rgr-lena1 <mhoranihirwe@gmail.com>
2025-09-21 18:19:52 +0200

    Update rwanda_coffee_ddl.sql

--- Principles of Business Analytics Project/rwanda_coffee_ddl.sql
+++ Principles of Business Analytics Project/rwanda_coffee_ddl.sql
@@ -10,7 +10,7 @@ CREATE TABLE Farmers (
 );
 
 -- Buyers table contains information of local and international buyers
-CREATE TABLE Buyers (
+CREATE TABLE buyers (
     buyer_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     country VARCHAR(50) NOT NULL,
@@ -19,18 +19,18 @@ CREATE TABLE Buyers (
 );
 
 -- Harvests table captures each coffee harvest linked to a buyer
-CREATE TABLE Harvests (
+CREATE TABLE harvests (
     harvest_id SERIAL PRIMARY KEY,
     farmer_id INT NOT NULL,
     harvest_date DATE NOT NULL,
     quantity_kg DECIMAL(10,2) CHECK (quantity_kg > 0),
     quality_score INT CHECK (quality_score BETWEEN 0 AND 100),
     batch_code VARCHAR(50) UNIQUE NOT NULL,
-    FOREIGN KEY (farmer_id) REFERENCES Farmers(farmer_id) ON DELETE CASCADE
+    FOREIGN KEY (farmer_id) REFERENCES farmers(farmer_id) ON DELETE CASCADE
 );
 
 -- Sales table links harvests with buyers and records transactions
-CREATE TABLE Sales (
+CREATE TABLE sales (
     sale_id SERIAL PRIMARY KEY,
     harvest_id INT NOT NULL,
     buyer_id INT NOT NULL,
@@ -39,6 +39,6 @@ CREATE TABLE Sales (
     price_per_kg DECIMAL(10,2) CHECK (price_per_kg > 0),
     total_amount DECIMAL(12,2) GENERATED ALWAYS AS (quantity_sold_kg * price_per_kg) STORED,
     invoice_no VARCHAR(50) UNIQUE NOT NULL,
-    FOREIGN KEY (harvest_id) REFERENCES Harvests(harvest_id) ON DELETE CASCADE,
-    FOREIGN KEY (buyer_id) REFERENCES Buyers(buyer_id) ON DELETE CASCADE
+    FOREIGN KEY (harvest_id) REFERENCES harvests(harvest_id) ON DELETE CASCADE,
+    FOREIGN KEY (buyer_id) REFERENCES buyers(buyer_id) ON DELETE CASCADE
 );
