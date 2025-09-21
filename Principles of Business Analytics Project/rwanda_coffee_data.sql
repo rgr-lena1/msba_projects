de7485fff21baf7288ed3fbdbc4afe9cc7e2d92d
rgr-lena1 <mhoranihirwe@gmail.com>
2025-09-21 17:30:27 +0200

    Update rwanda_coffee_data.sql

--- Principles of Business Analytics Project/rwanda_coffee_data.sql
+++ Principles of Business Analytics Project/rwanda_coffee_data.sql
@@ -5,9 +5,9 @@ INSERT INTO Farmers (farmer_id, name, district, cooperative_join_date) VALUES
 (3, 'Elie Nkurunziza', 'Gakenke', '2018-11-10'),
 (4, 'Marie Uwitonze', 'Nyamasheke', '2021-02-05'),
 (5, 'Patrick Habimana', 'Karongi', '2019-12-30'),
-(6, 'Jeanne Uwamahoro', 'Ruhango', '2022-01-15'),
+(6, 'Jacques Mahoro', 'Ruhango', '2022-01-15'),
 (7, 'Emma Kayitesi', 'Rutsiro', '2020-07-22'),
-(8, 'Claudine Iradukunda', 'Rusizi', '2021-09-10'),
+(8, 'Claude Iradukunda', 'Rusizi', '2021-09-10'),
 (9, 'Fabrice Munyaneza', 'Nyamagabe', '2018-05-25'),
 (10, 'Claire Mukarwego', 'Nyamagabe', '2018-05-25');
 
